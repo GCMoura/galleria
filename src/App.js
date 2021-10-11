6f315fc46ba99c857691e72dc7453814fce62c47
Gabriel Moura <gabrielmoura29@gmail.com>
2021-10-11 17:54:03 -0300

    ok

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react'
-import Paint from './components/Paint'
 
 function App() {
 
--- src/components/Paint/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Paint({ src }) {
-  return (
-    <img src={src} width="300" height="150" alt=""  />
-  )
-}
-
-export default Paint
