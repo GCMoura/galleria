222057fea8a0f267b68defb915b462509e294922
Gabriel Moura <gabrielmoura29@gmail.com>
2021-10-13 11:13:29 -0300

    change search by artist

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,28 +1,97 @@
 import { useState } from 'react'
-import { useHistory } from 'react-router'
+
+import '../../styles/loader.scss'
 
 function Home() {
-
-  const history = useHistory()
-
+  
+  const BASE_URL = window.location.hostname.includes('localhost')
+  ? 'http://localhost:5000'
+  : 'https://rijksmuseum.herokuapp.com'
+  
+  const [storeImages, setStoreImages] = useState([])
+  const [storeTitle, setStoreTitle] = useState([])
   const [artist, setArtist] = useState('')
 
-  function showGalleria(event){
+  const arrArt = []
+  const arrArtTitle = []
+  var page = 1
+
+  function handleSubmit(event){
     event.preventDefault()
-    console.log(artist)
-    history.push({
-      pathname: "/galleria",
-      state: {
-        artist: artist,
-      }
-    })
+    document.querySelector('.loader').style.display = "block"
+    getArt()
+  }
+  
+  function getArt(){
+    
+    try {
+      fetch(`${BASE_URL}/${page}`)
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        
+        for(let index in data.artObjects){  
+          let id = data.artObjects[index].id.split('-')
+          if(id[1] === 'SK'){
+            if(data.artObjects[index].webImage !== null && data.artObjects[index].hasImage){
+              if( data.artObjects[index].principalOrFirstMaker === artist){
+                arrArt.push(data.artObjects[index].webImage.url)
+                arrArtTitle.push(data.artObjects[index].title)
+              }
+            }
+          }
+        }
+        
+        page++
+        if(page <= Math.floor(data.count / 100 + 1) && page <= 7){
+          getArt()
+        } else {
+          page = 1
+          document.querySelector('.loader').style.display = "none"
+          setStoreImages(arrArt)
+          setStoreTitle(arrArtTitle)
+        }
+
+      })
+    } catch (error) {
+      console.log(error)
+    }
   }
 
-  return(
-    <form onSubmit={showGalleria}>
-      <input type='submit' value="Rembrandt" onClick={(event) => { setArtist(event.target.value) }} />
-      <input type='submit' value="Vermeer" onClick={(event) => { setArtist(event.target.value) }} />
-    </form>
-  )
+  return (
+    <>
+      <h1>Galleria</h1>
+      
+      <form onSubmit={handleSubmit}>
+        <input type='submit' value="Rembrandt van Rijn" onClick={(event) => { setArtist(event.target.value) }} />
+        <input type='submit' value="Johannes Vermeer" onClick={(event) => { setArtist(event.target.value) }} />
+        <input type='submit' value="Vincent van Gogh" onClick={(event) => { setArtist(event.target.value) }} />
+        <input type='submit' name="Jan Havicksz. Steen" value="Jan Steen" onClick={(event) => { setArtist(event.target.name) }} />
+      </form> 
+
+      <div class="loader">
+        <div class="loader__bar"></div>
+        <div class="loader__bar"></div>
+        <div class="loader__bar"></div>
+        <div class="loader__bar"></div>
+        <div class="loader__bar"></div>
+        <div class="loader__ball"></div>
+        <h4>Loading...</h4>
+      </div>
+
+      {
+        storeImages.map((img, index) => {
+          
+          return (
+            <main key={index}>
+              <img  src={img} alt='' width='300' height='300' />
+              <h3> {storeTitle[index] } </h3>
+            </main>
+          )
+        })
+      }
+    </>
+  );  
 }
 export default Home;
--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,12 @@ import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import Home from './pages/Home'
-import Galleria from './pages/Galleria'
 
 function Routes(){
   return(
     <BrowserRouter>
       <Switch>
         <Route path="/" component={Home} exact />
-        <Route path="/galleria" component={Galleria} />
       </Switch>
     </BrowserRouter>
   )
